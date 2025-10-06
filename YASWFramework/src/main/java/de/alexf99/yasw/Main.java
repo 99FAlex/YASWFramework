4b35ce85bcabffce892d9e660d5d56298df4ba08
99FAlex <felixgeissler99@gmail.com>
2025-10-06 18:22:51 +0200

    refactor: Remove server setup boilerplate from Main class

--- YASWFramework/src/main/java/de/alexf99/yasw/Main.java
+++ YASWFramework/src/main/java/de/alexf99/yasw/Main.java
@@ -3,19 +3,8 @@ package de.alexf99.yasw;
 import de.alexf99.yasw.httpServer.SetView;
 import de.alexf99.yasw.httpServer.YaswHttpServer;
 
-import java.io.IOException;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        YaswHttpServer.init(8000);
-        YaswHttpServer.server.createContext("/", new SetView("index.html"));
-        YaswHttpServer.server.start();
 
     }
-
-
-
-
 }
