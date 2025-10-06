dbf74384554e56fc81ba3509b67b722446594042
99FAlex <felixgeissler99@gmail.com>
2025-10-06 17:09:07 +0200

    Initializes basic HTTP server
    Sets up a basic HTTP server using YASW framework.
    Includes a simple route to serve static HTML.
    Adds JUnit dependency for testing.

--- YASWFramework/src/main/java/de/alexf99/yasw/Main.java
+++ YASWFramework/src/main/java/de/alexf99/yasw/Main.java
@@ -1,17 +1,21 @@
 package de.alexf99.yasw;
 
+import de.alexf99.yasw.httpServer.SetView;
+import de.alexf99.yasw.httpServer.YaswHttpServer;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws Exception {
+        YaswHttpServer.init(8000);
+        YaswHttpServer.server.createContext("/", new SetView("index.html"));
+        YaswHttpServer.server.start();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
+
+
+
 }
